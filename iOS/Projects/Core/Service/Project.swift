c4dc7e1971e07667bba9631238ad9a0426da0a94
wnsxor1993 <wnsxor1993@naver.com>
2023-10-09 20:34:07 +0900

    chore: RxCocoa, RxRelay 추가 및 Core 적용

--- iOS/Projects/Core/Service/Project.swift
+++ iOS/Projects/Core/Service/Project.swift
@@ -14,6 +14,7 @@ let project: Project = .makeProject(
     packages: [],
     dependencies: [
         .SPM.RxSwift.dependency,
+        .SPM.RxRelay.dependency,
         .Module.type(.core(subModule: .entity)).dependency
     ],
     hasTests: false
